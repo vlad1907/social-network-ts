d8e764e1400f572a2882a49eb320f554082e32f5
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-02-02 22:53:58 +0200

    12L

---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img
+                src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+            </div>
+            <div>
+                ava+descript
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
