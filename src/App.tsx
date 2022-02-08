8eed3488b7eac04c7152423c81eba798b564e419
vlad1907 <58011613+vlad1907@users.noreply.github.com>
2022-02-08 22:40:29 +0200

    route and path

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-
-                {/*<Dialogs/>*/}
-                <Profile/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
